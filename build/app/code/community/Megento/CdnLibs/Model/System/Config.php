f89660c7f1817f5d427a77f75a3dcb3d39eb77bc
Ngoc Anh Doan <ngoc@nhdoan.de>
2012-05-06 19:26:04 +0200

    Refactored namespace.

---
+++ build/app/code/community/Megento/CdnLibs/Model/System/Config.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE_AFL.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category  Megento
+ * @package   Megento_CdnLibs
+ * @author    Ngoc Anh Doan <ngoc@nhdoan.de>
+ * @copyright 2012 Ngoc Anh Doan
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      https://github.com/ngocanh/megento_cdnlibs
+ */
+
+/**
+ * Configuration class for all settings underneath 'nd_cdn_libs'.
+ *
+ * @category  Megento
+ * @package   Megento_CdnLibs
+ * @author    Ngoc Anh Doan <ngoc@nhdoan.de>
+ * @copyright 2012 Ngoc Anh Doan
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      https://github.com/ngocanh/megento_cdnlibs
+ */
+class Megento_CdnLibs_Model_System_Config extends Megento_CdnLibs_Model_System_Config_Abstract
+{
+    /**
+     * Loading system XML section of <i>nd_cdn_libs</i>
+     *
+     * @var string
+     */
+    protected $_xmlPath = 'nd_cdn_libs';
+}
